edecd6ebb9e3abd5d01a03c9836f7cff888a39f9
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-10-11 18:49:53 +0200

    Finish profile and nav

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useUserSession from "src/modules/useUserSession";
 import {Notify} from "quasar";
-import {ErrorCodeEnum} from "src/api/errorCodeEnum";
+import {ErrorCodeEnum, getJwtErrorCodes} from "src/api/errorCodeEnum";
 const AxiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   timeout: 10000,
@@ -17,7 +17,7 @@ AxiosInstance.interceptors.request.use(
   request => {
     const userSession = useUserSession()
     if (userSession.isLoggedIn) {
-      request.headers.Authorization = `Bearer ${userSession.token}`
+      request.headers.Authorization = `${userSession.token}`
     }
     return request
   },
@@ -32,7 +32,6 @@ AxiosInstance.interceptors.response.use(
     // if (response.config.responseType === RESPONSE_TYPES.ARRAYBUFFER) return response.data;
     // if (response.data.type === RESPONSE_TYPES.PDF) return response;
     // For now
-    return response.data
     const {
       data: { data },
     } = response;
@@ -40,9 +39,13 @@ AxiosInstance.interceptors.response.use(
   },
   async (error) => {
     const errMsg = error.response.data?.message ?? 'Unknown error occurred. Please try again later.'
-    const code = error.response.data?.code ?? null
+    const httpCode = error.response.status
+    const code = error.response.data?.code ?? '-'
 
-    if (code === ErrorCodeEnum.UNAUTHORIZED) {
+    if (
+      httpCode === 401
+      && getJwtErrorCodes().includes(code)
+    ) {
       localStorage.removeItem('token')
       localStorage.removeItem('user')
       window.location.href = '/'
--- src/modules/UserProfile/useChangePassword.js
+++ src/modules/UserProfile/useChangePassword.js
@@ -5,7 +5,7 @@ import userApi from "src/api/userApi";
 export default function () {
   const $q = useQuasar()
   const isLoading = ref(false)
-  const data = reactive({})
+  const data = ref({})
   const form = ref(null)
 
   const changePassword = async () => {
@@ -16,7 +16,9 @@ export default function () {
     isLoading.value = true
 
     try {
-      await userApi.changePassword(data)
+      await userApi.changePassword(data.value)
+      data.value = {}
+      form.value.resetValidation()
       $q.notify({ type: 'positive', message: 'Password changed' })
     } finally {
       isLoading.value = false
