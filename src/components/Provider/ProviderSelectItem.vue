1fa5d1d269c9c77f2931d58c44e6ea00f05e558f
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-10-30 13:02:28 +0100

    Fix onedrive logo

--- src/components/Provider/ProviderSelectItem.vue
+++ src/components/Provider/ProviderSelectItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-center items-center q-pa-sm">
-    <q-img :src="provider.logo" width="30px" class="q-mr-sm"/> {{ provider.name }}
+    <img :src="provider.logo" class="q-mr-sm" style="width: 30px"/> {{ provider.name }}
   </div>
 </template>
 
