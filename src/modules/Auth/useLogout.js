99cc98cb75d68e2614b09fff673e3bfdfd37f6d6
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-11-19 10:43:16 +0100

    Added tests

--- src/modules/Auth/useLogout.js
+++ src/modules/Auth/useLogout.js
@@ -1,16 +1,15 @@
 import useUserSession from "src/modules/useUserSession";
 import {useRouter} from "vue-router";
-import {useQuasar} from "quasar";
+import Quasar from "quasar";
 
 export default function () {
   const userSession = useUserSession()
   const router = useRouter()
-  const $q = useQuasar()
 
   const logout = async () => {
     userSession.token = ''
     userSession.user = {}
-    $q.notify({ type: 'positive', message: 'Logged out!' })
+    Quasar.Notify.create({ type: 'positive', message: 'Logged out!' })
     await router.push({ name: 'login' })
   }
 
