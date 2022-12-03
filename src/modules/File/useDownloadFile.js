097c270a437620c64cf902153b31b8e03b50445f
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-12-03 17:22:41 +0100

    More tests

--- src/modules/File/useDownloadFile.js
+++ src/modules/File/useDownloadFile.js
@@ -1,15 +1,13 @@
-import {ref} from "vue";
+import { ref } from "vue";
 import fileApi from "src/api/fileApi.js";
-import {useQuasar} from "quasar";
+import Quasar from "quasar";
 
-const BLOCK_DOWNLOAD_TRIES = 3;
-const RETRY_DOWNLOAD_HTTP_CODE = 449;
+export const BLOCK_DOWNLOAD_TRIES = 3;
 
-const downloadingFile = ref()
+const downloadingFile = ref();
 
-export default function() {
-  const isLoading = ref(false)
-  const $q = useQuasar()
+export default function () {
+  const isLoading = ref(false);
 
   const saveAs = ({ name, buffers, mime = "application/octet-stream" }) => {
     const blob = new Blob([buffers], { type: mime });
@@ -19,7 +17,7 @@ export default function() {
     a.href = blobUrl;
     a.click();
     URL.revokeObjectURL(blob);
-  }
+  };
 
   const getBlock = async (file, block) => {
     let isSuccess = false;
@@ -31,16 +29,16 @@ export default function() {
       tryNumber++;
 
       try {
-        result = await fileApi.downloadBlock(block.uuid, file.uuid)
-        isSuccess = true
+        result = await fileApi.downloadBlock(block.uuid, file.uuid);
+        isSuccess = true;
       } catch (e) {
         error = e;
       }
-    } while (!isSuccess && tryNumber <= BLOCK_DOWNLOAD_TRIES)
+    } while (!isSuccess && tryNumber <= BLOCK_DOWNLOAD_TRIES);
 
-    if (isSuccess) return result
-    else throw error
-  }
+    if (isSuccess) return result;
+    else throw error;
+  };
 
   const concatenate = (arrays) => {
     if (!arrays.length) return null;
@@ -52,61 +50,71 @@ export default function() {
       length += array.length;
     }
     return result;
-  }
+  };
 
   const downloadSingle = async (file, block) => {
-    const buffers = await getBlock(file, block)
-    saveAs({ name: file.name, buffers })
-  }
+    const buffers = await getBlock(file, block);
+    saveAs({ name: file.name, buffers });
+  };
 
   const downloadMultiple = async (file, blocks) => {
-    return await Promise.all(blocks.map(block => new Promise(async (resolve) => {
-      const buffer = await getBlock(file, block)
-      resolve({
-        order: block.order,
-        buffer
-      })
-    })))
-  }
+    return await Promise.all(
+      blocks.map(
+        (block) =>
+          new Promise(async (resolve) => {
+            const buffer = await getBlock(file, block);
+            resolve({
+              order: block.order,
+              buffer,
+            });
+          })
+      )
+    );
+  };
 
   const saveMultiple = (file, blockBuffers) => {
     const sortedBuffers = blockBuffers
-      .sort((a, b) => a.order > b.order ? 1 : -1)
+      .sort((a, b) => (a.order > b.order ? 1 : -1))
       .map((item) => new Uint8Array(item.buffer));
 
     const buffers = concatenate(sortedBuffers);
-    saveAs({ name: file.name, buffers})
-  }
+    saveAs({ name: file.name, buffers });
+  };
 
   const download = async (file) => {
     if (downloadingFile.value) {
-      $q.notify({ type: 'negative', message: 'Only single file can be downloaded at the time' })
-      return
+      Quasar.Notify.create({
+        type: "negative",
+        message: "Only single file can be downloaded at the time",
+      });
+      return;
     }
 
-    isLoading.value = true
-    downloadingFile.value = file
+    isLoading.value = true;
+    downloadingFile.value = file;
 
     try {
-      const result = await fileApi.downloadFile(file.uuid)
-      const returnedFile = result.file
+      const result = await fileApi.downloadFile(file.uuid);
+      const returnedFile = result.file;
 
       if (result.blocks.length === 1) {
-        await downloadSingle(returnedFile, result.blocks[0])
+        await downloadSingle(returnedFile, result.blocks[0]);
       } else {
-        const blockBuffers = await downloadMultiple(returnedFile, result.blocks)
-        await saveMultiple(file, blockBuffers)
+        const blockBuffers = await downloadMultiple(
+          returnedFile,
+          result.blocks
+        );
+        await saveMultiple(file, blockBuffers);
       }
-
     } finally {
-      isLoading.value = false
-      downloadingFile.value = null
+      isLoading.value = false;
+      downloadingFile.value = null;
     }
-  }
+  };
 
   return {
     isLoading,
     downloadingFile,
     download,
-  }
+  };
 }
